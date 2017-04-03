c1d0f22d0165d01ee24f498828c660c96895724f
Vitor Falcao <vitorfhcosta@gmail.com>
2017-04-03 11:26:17 -0300

    Add images and login front end started

---
+++ ForumDEG/ForumDEG/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumDEG.ViewModels {
+    class LoginViewModel {
+        public bool MakeLogin(string email, string password) {
+            if (!email.Equals("") && password.Equals(email)) return true;
+            return false;
+        }
+    }
+}
