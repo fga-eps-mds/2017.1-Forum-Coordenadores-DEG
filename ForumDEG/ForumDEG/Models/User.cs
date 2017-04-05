88f79b58b97fc62b73197078f69bbb302f92423d
Lucas Rufino <lucaslrt@outlook.com>
2017-04-04 21:46:48 -0300

    Create User class.

---
+++ ForumDEG/ForumDEG/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumDEG.Models
+{
+    public class User {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string nome { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public User () { 
+
+        }
+    }
+}
---
+++ ForumDEG/ForumDEG/Utils/InterfaceSQLite.cs
@@ -0,0 +1,14 @@
+﻿using SQLite.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumDEG.Utils
+{
+    interface InterfaceSQLite
+    {
+        SQLiteConnection GetConnection();
+    }
+}
