6c301f782355c9af6bf582df6afe58926e36ff49
Vitor Falcao <vitorfhcosta@gmail.com>
2017-04-08 12:54:38 -0300

    Fixed to correspond to MVVM architecture.

--- ForumDEG/ForumDEG/ViewModels/ForunsViewModel.cs
+++ ForumDEG/ForumDEG/ViewModels/ForunsViewModel.cs
@@ -9,6 +9,30 @@ using System.Collections.ObjectModel;
 
 namespace ForumDEG.ViewModels {
     class ForunsViewModel {
-        
+        private List<Forum> _foruns;
+
+        public ForunsViewModel() {
+            _foruns = new List<Forum>();
+        }
+
+        public List<Forum> GetUpdatedList() {
+            if (_foruns.Count == 0) {
+                List<string> schedules = new List<string> {
+                    "Pauta 01",
+                    "Pauta 02",
+                    "Pauta 03"
+                };
+                _foruns = new List<Forum> { 
+                    //example
+                    new Forum { _title = "Forum 01", _place = "Place 01", _date = new DateTime(2017, 8, 24), _schedules = schedules },
+                    new Forum { _title = "Forum 01", _place = "Place 01", _date = new DateTime(2017, 8, 24), _schedules = schedules },
+                    new Forum { _title = "Forum 01", _place = "Place 01", _date = new DateTime(2017, 8, 24), _schedules = schedules },
+                    new Forum { _title = "Forum 01", _place = "Place 01", _date = new DateTime(2017, 8, 24), _schedules = schedules },
+                    new Forum { _title = "Forum 01", _place = "Place 01", _date = new DateTime(2017, 8, 24), _schedules = schedules },
+                    new Forum { _title = "Forum 01", _place = "Place 01", _date = new DateTime(2017, 8, 24), _schedules = schedules }
+                };
+            }
+            return _foruns;
+        }
     }
 }
--- ForumDEG/ForumDEG/Views/ForunsPage.xaml.cs
+++ ForumDEG/ForumDEG/Views/ForunsPage.xaml.cs
@@ -7,35 +7,22 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using ForumDEG.ViewModels;
-using ForumDEG.Models;
 
 namespace ForumDEG.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ForunsPage : MasterDetailPage {
-        //Simulatio
+        private ForunsViewModel _fvm;
         
-        List<Forum>  GetForum() {
-            return new List<Forum> { 
-                //example
-                new Forum {_title = "Forum 01", _place ="Auditorio1"},
-                new Forum {_title = "Forum 02", _place ="Auditorio2"},
-                new Forum {_title = "Forum 03", _place ="Auditorio3"},
-                new Forum {_title = "Forum 04", _place ="Auditorio4"},
-                new Forum {_title = "Forum 05", _place ="Auditorio5"},
-                new Forum {_title = "Forum 06", _place ="Auditorio6"},
-                new Forum {_title = "Forum 07", _place ="Auditorio7"}
-            };
-        }
-        // ------ until Here
-        void Handle_Refreshing(object sender, System.EventArgs e) {
-            ForumList.ItemsSource = GetForum();
-            ForumList.EndRefresh();
-        }
-
         public ForunsPage() {
             InitializeComponent();
-            
-            ForumList.ItemsSource = GetForum();
+
+            _fvm = new ForunsViewModel();
+            ForumList.ItemsSource = _fvm.GetUpdatedList();
+        }
+
+        void Handle_Refreshing(object sender, System.EventArgs e) {
+            ForumList.ItemsSource = _fvm.GetUpdatedList();
+            ForumList.EndRefresh();
         }
 
         void Handle_Selected(object sender, System.EventArgs e) {
@@ -69,10 +56,6 @@ namespace ForumDEG.Views {
 
         void Handle_ClickedCriarFormulario(object sender, System.EventArgs e) {
             //            Implementation here
-        }
-
-        void Handle_ClieckedPauta(object sender, System.EventArgs e) {
-
         }
     }
 }
