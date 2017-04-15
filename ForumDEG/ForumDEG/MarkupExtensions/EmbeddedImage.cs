171434ecdcbcae270e13c1a575fb62c6aacff322
Vitor Falcao <vitorfhcosta@gmail.com>
2017-04-15 17:22:00 -0300

    Added support to circular embedded images

---
+++ ForumDEG/ForumDEG/MarkupExtensions/EmbeddedImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+namespace ForumDEG.MarkupExtensions {
+    class EmbeddedImage : IMarkupExtension {
+        public string ResourceId { get; set; } 
+
+        public object ProvideValue(IServiceProvider serviceProvider) {
+            if(String.IsNullOrWhiteSpace(ResourceId)) return null;
+            return ImageSource.FromResource(ResourceId);
+        }
+    }
+}
