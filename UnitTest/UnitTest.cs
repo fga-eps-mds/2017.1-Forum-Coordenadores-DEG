f890724f51ad5b2eb12fe4a5eddbf45d3ebf32b8
Clarissa Borges <clarissalimab08@gmail.com>
2017-04-15 18:42:36 -0300

    Add UnitTest initial file

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+using ForumDEG;
+
+namespace UnitTest {
+    public class Test {
+
+        [Test()]
+        public void Test1() {
+            Assert.True(true);
+        }
+    }
+}
