b3265391cc182731826725eb78cef1aabc5e9782
Vitor Falcao <vitorfhcosta@gmail.com>
2017-04-18 18:12:45 -0300

    [MVVM] Created classes and interface
      - Created interface IPageService
      - Created PageService
      - Created BaseViewModel

--- ForumDEG/ForumDEG/ViewModels/PageService.cs
+++ ForumDEG/ForumDEG/ViewModels/PageService.cs
@@ -5,16 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-
 namespace ForumDEG.ViewModels {
     class PageService : IPageService {
-
-        public async Task DisplayAlert(string title, string message, string ok) {
-            await Application.Current.MainPage.DisplayAlert(title, message, ok);
+        public async Task<bool> DisplayAlert(string title, string message, string ok, string cancel) {
+            return await Application.Current.MainPage.DisplayAlert(title, message, ok, cancel);
         }
 
-        public Task PushAsync(Page page) {
-            throw new NotImplementedException();
+        public async Task PushAsync(Page page) {
+            await Application.Current.MainPage.Navigation.PushAsync(page);
         }
     }
 }
