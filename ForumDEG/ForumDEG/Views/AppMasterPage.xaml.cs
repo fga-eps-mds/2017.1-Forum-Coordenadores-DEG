295c3a17d4d1171dac30c7afeea0b53dc222b1b3
Vitor Falcao <vitorfhcosta@gmail.com>
2017-04-19 19:59:27 -0300

    Created ViewModel to AppMasterPage

--- ForumDEG/ForumDEG/Views/AppMasterPage.xaml.cs
+++ ForumDEG/ForumDEG/Views/AppMasterPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ForumDEG.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,30 +11,11 @@ using Xamarin.Forms.Xaml;
 namespace ForumDEG.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AppMasterPage : MasterDetailPage {
+        private AppMasterViewModel viewModel = new AppMasterViewModel(new PageService());
+
         public AppMasterPage() {
+            BindingContext = viewModel;
             InitializeComponent();
-
-        }
-
-        async void Handle_ClickedForum(object sender, System.EventArgs e) {
-            await Navigation.PushAsync(new ForumsPage());
-        }
-        void Handle_ClickedCriarForum(object sender, System.EventArgs e) {
-            //            Implementation here
-        }
-
-        void Handle_ClickedCoordenador(object sender, System.EventArgs e) {
-            //            Implementation here
-        }
-
-        void Handle_ClickedCadastrarCoordenador(object sender, System.EventArgs e) {
-            //            Implementation here
-        }
-        void Handle_ClickedFormulario(object sender, System.EventArgs e) {
-            //            Implementation here
-        }
-        void Handle_ClickedCriarFormulario(object sender, System.EventArgs e) {
-            //            Implementation here
         }
     }
 }
