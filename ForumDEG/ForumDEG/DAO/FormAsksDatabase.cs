e8ccb95f144c76613dde22526dd0f3c22e51d352
Lucas Rufino <lucaslrt@outlook.com>
2017-04-25 13:06:54 -0300

    Creating FormAsks Database.

---
+++ ForumDEG/ForumDEG/DAO/FormAsksDatabase.cs
@@ -0,0 +1,38 @@
+﻿using ForumDEG.Models;
+using ForumDEG.Utils;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ForumDEG.DAO {
+    class FormAsksDatabase {
+
+        readonly SQLiteAsyncConnection _database;
+
+        private static FormAsksDatabase _formAsksDatabase = null;
+
+        private FormAsksDatabase(string databasePath) {
+            _database = new SQLiteAsyncConnection(databasePath);
+
+            _database.CreateTableAsync<FormAsks>().Wait();
+            Debug.WriteLine("FormAsksDatabase: Database created.");
+        }
+
+        public static FormAsksDatabase getFormDB {
+            get {
+                if (_formAsksDatabase == null) {
+                    _formAsksDatabase = new FormAsksDatabase(DependencyService.Get<InterfaceSQLite>().GetLocalFilePath("FormAsks.db3"));
+                    Debug.WriteLine("FormAsksDatabase getFormDB: _formAsksDatabase getted.");
+                }
+
+                Debug.WriteLine("FormAsksDatabase getFormDB: returning _formAsksDatabase.");
+                return _formAsksDatabase;
+            }
+        }
+    }
+}
---
+++ ForumDEG/ForumDEG/Models/FormAsks.cs
@@ -0,0 +1,31 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumDEG.Models {
+    public class FormAsks {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Indexed]
+        public int FormId { get; set; }
+
+        public int AskType { get; set; }
+
+        public List<String> Asks { get; set; }
+
+        public List<String> Answers { get; set; }
+
+        // Override ToString to debug database.
+        /* Usage:
+         * Debug.WriteLine(form.ToString());
+        */
+        public override string ToString() {
+
+            return string.Format("[ForumAsks: ID={0}, FormId={1}, AskType={2}, Asks={3}, Answers={4}]", Id, FormId, AskType, Asks.ToList(), Answers.ToList());
+        }
+    }
+}
