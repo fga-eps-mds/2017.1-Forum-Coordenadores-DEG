d410531c51e5f82218f95beb6d16f292df66842b
Vitor Falcao <vitorfhcosta@gmail.com>
2017-05-02 17:21:13 -0300

    Created ViewModel and View

---
+++ ForumDEG/ForumDEG/Views/ChangePasswordView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ForumDEG.Views {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChangePasswordView : ContentPage {
+        public ChangePasswordView() {
+            InitializeComponent();
+        }
+    }
+}
