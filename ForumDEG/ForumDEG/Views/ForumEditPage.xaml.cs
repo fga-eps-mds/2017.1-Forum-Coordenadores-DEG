0fc1a419e8bae1ad840271bdf72f17e7d2650ef5
Breno Mariz <mrzbeo@gmail.com>
2017-05-05 21:50:26 -0300

    [EDIT FURUM] creating ForumEditPage

---
+++ ForumDEG/ForumDEG/Views/ForumEditPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ForumDEG.ViewModels;
+
+namespace ForumDEG.Views {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ForumEditPage : ContentPage {
+        private ForumEditViewModel _viewModel = new ForumEditViewModel(new PageService());
+        public ForumEditPage() {
+            InitializeComponent();
+
+            BindingContext = _viewModel;
+
+            dateDatePicker.Date = DateTime.Now;
+            dateDatePicker.MinimumDate = DateTime.Now;
+        }
+    }
+}
