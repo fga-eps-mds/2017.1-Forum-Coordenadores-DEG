52c5e020e1977c21a1cbec31e3819a7dd36ca81f
Vitor Falcao <vitorfhcosta@gmail.com>
2017-05-09 18:20:36 -0300

    Implementation of backend methods
    Signed-off-by: Vitor Falcao <vitorfhcosta@gmail.com>
    Signed-off-by: Arthur Assis <arhur120496@gmail.com>

--- ForumDEG/ForumDEG/ViewModels/LoginViewModel.cs
+++ ForumDEG/ForumDEG/ViewModels/LoginViewModel.cs
@@ -1,10 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Acr.UserDialogs;
+using ForumDEG.Interfaces;
+using System.Diagnostics;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ForumDEG.ViewModels {
     class LoginViewModel {
+        public string _userEmail { get; set; }
+        public string _userPassword { get; set; }
+
+        public ICommand MakeLoginCommand { get; private set; }
+
+        private IPageService _pageService;
+        private IUserDialogs _dialog;
+
+        public LoginViewModel(IPageService pageService, IUserDialogs dialog) {
+            _pageService = pageService;
+            _dialog = dialog;
+
+            MakeLoginCommand = new Command(MakeLogin);
+        }
+
+        private void MakeLogin() {
+            // executed when login button is clicked
+            if (!IsAnyFieldEmpty()) return;
+            if (!ValidateEmailRegex()) return;
+            if (!ValidatePasswordRegex()) return;
+            if (!ValidateOnDatabase()) return;
+        }
+
+        private void LogUser() {
+
+        }
+
+        private bool IsAnyFieldEmpty() {
+            if (string.IsNullOrWhiteSpace(_userEmail) || string.IsNullOrWhiteSpace(_userPassword)) {
+                return true;
+            }
+            return false;
+        }
+
+        private bool ValidateEmailRegex() {
+            // validates email regex
+            return true;
+        }
+
+        private bool ValidatePasswordRegex() {
+            // validates password regex
+            return true;
+        }
+
+        private bool ValidateOnDatabase() {
+            // validate if user and password matches any in database
+            return true;
+        }
     }
 }
--- ForumDEG/ForumDEG/Views/LoginPage.xaml.cs
+++ ForumDEG/ForumDEG/Views/LoginPage.xaml.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Acr.UserDialogs;
+using ForumDEG.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ForumDEG.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage {
+        private LoginViewModel _viewModel;
+
         public LoginPage() {
+            _viewModel = new LoginViewModel(new PageService(), UserDialogs.Instance);
+            BindingContext = _viewModel;
+
             InitializeComponent();
         }
     }
