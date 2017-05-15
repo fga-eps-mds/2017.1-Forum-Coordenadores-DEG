9d53f845a04dcd16d7fbcc84d8737563586bb4a3
Felipe Hargreaves <fhargreaves00@gmail.com>
2017-05-15 20:07:09 -0300

    [FRONT-END] Created NewFormsPage.
    Signed-off-by: arthur0496 <arthur120496@gmail.com>
    Signed-off-by: Felipe Hargreaves <fhargreaves00@gmail.com>

---
+++ ForumDEG/ForumDEG/ViewModels/NewFormViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ForumDEG.ViewModels {
+    class NewFormViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand PlusButtonClickedCommand { get; set; }
+
+        private float TapCount = 0;
+
+        public float _tapCount {
+            get {
+                return _tapCount;
+            }
+            set {
+                if (_tapCount != value)
+                    _tapCount = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TapCount"));
+            }
+        }
+
+        private bool _extraButtonsVisibility;
+
+        public bool ExtraButtonsVisibility {
+            get {
+                return _extraButtonsVisibility;
+            }
+            set {
+                if (_extraButtonsVisibility != value) {
+                    _extraButtonsVisibility = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ExtraButtonsVisibility"));
+                }
+            }
+        }
+
+        public NewFormViewModel() {
+            PlusButtonClickedCommand = new Command(async () => await PlusButtonClicked());
+
+            ExtraButtonsVisibility = false;
+            TapCount = 0;
+        }
+
+        private async Task PlusButtonClicked() {
+            TapCount++;
+            if (TapCount % 2 == 0) {
+                ExtraButtonsVisibility = false;
+            } else {
+                ExtraButtonsVisibility = true;
+            }
+        }
+    }
+}
---
+++ ForumDEG/ForumDEG/Views/Forms/NewFormPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ForumDEG.ViewModels;
+
+namespace ForumDEG.Views.Forms {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewFormPage : ContentPage {
+        public NewFormPage() {
+            InitializeComponent();
+            BindingContext = new NewFormViewModel();
+        }
+    }
+}
