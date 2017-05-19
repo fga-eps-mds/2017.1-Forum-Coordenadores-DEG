9077e44d4ac23e5b952021a02766fe3150941175
Vitor Falcao <vitorfhcosta@gmail.com>
2017-05-19 15:26:01 -0300

    Created Page and ViewModel
    Signed-off-by: Vitor Falcao <vitorfhcosta@gmail.com>
    Signed-off-by: Thiago Moreira <thiago.cmoreira@yahoo.com.br>

---
+++ ForumDEG/ForumDEG/Views/Forms/NewDiscursiveQuestionPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ForumDEG.Views.Forms {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewDiscursiveQuestionPage : ContentPage {
+        public NewDiscursiveQuestionPage() {
+            InitializeComponent();
+        }
+    }
+}
