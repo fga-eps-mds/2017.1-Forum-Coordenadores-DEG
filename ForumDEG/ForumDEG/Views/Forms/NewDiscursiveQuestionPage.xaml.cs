178aa0c203152f556734cd54e6e1c068ebc14545
Vitor Falcao <vitorfhcosta@gmail.com>
2017-05-19 15:30:02 -0300

    Implemented View
    Signed-off-by: Vitor Falcao <vitorfhcosta@gmail.com>
    Signed-off-by: Thiago Moreira <thiago.cmoreira@yahoo.com.br>

--- ForumDEG/ForumDEG/Views/Forms/NewDiscursiveQuestionPage.xaml.cs
+++ ForumDEG/ForumDEG/Views/Forms/NewDiscursiveQuestionPage.xaml.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Acr.UserDialogs;
+using ForumDEG.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ForumDEG.Views.Forms {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewDiscursiveQuestionPage : ContentPage {
+        public NewDiscursiveQuestionViewModel _viewModel { get; private set; }
+
         public NewDiscursiveQuestionPage() {
+            _viewModel = new NewDiscursiveQuestionViewModel(new PageService(), UserDialogs.Instance);
+            BindingContext = _viewModel;
             InitializeComponent();
+        }
+
+        public void AddQuestion() {
+
         }
     }
 }
