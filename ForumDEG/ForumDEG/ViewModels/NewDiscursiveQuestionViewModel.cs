3b8cce88fd45627696bc0e1058796fc708fff50e
Vitor Falcao <vitorfhcosta@gmail.com>
2017-05-19 15:29:15 -0300

    Implemented ViewModel with validation
    Signed-off-by: Vitor Falcao <vitorfhcosta@gmail.com>
    Signed-off-by: Thiago Moreira <thiago.cmoreira@yahoo.com.br>

--- ForumDEG/ForumDEG/ViewModels/NewDiscursiveQuestionViewModel.cs
+++ ForumDEG/ForumDEG/ViewModels/NewDiscursiveQuestionViewModel.cs
@@ -1,10 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Acr.UserDialogs;
+using ForumDEG.Interfaces;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ForumDEG.ViewModels {
-    class NewDiscursiveQuestionViewModel {
+    public class NewDiscursiveQuestionViewModel {
+        public string _questionText { get; set; }
+
+        public ICommand AddQuestionCommand { get; set; }
+        public ICommand CancelQuestionCommand { get; set; }
+
+        private readonly IPageService _page;
+        private readonly IUserDialogs _dialog;
+
+        public NewDiscursiveQuestionViewModel(IPageService page, IUserDialogs dialog) {
+            _page = page;
+            _dialog = dialog;
+
+            AddQuestionCommand = new Command(async () => await AddQuestion());
+            CancelQuestionCommand = new Command(async () => await CancelQuestion());
+        }
+
+        private async Task AddQuestion() {
+            if (!VerifyQuestionField()) return;
+            await _page.PopAsync();
+        }
+
+        private async Task CancelQuestion() {
+            await _page.PopAsync();
+        }
+
+        private bool VerifyQuestionField() {
+            if (string.IsNullOrWhiteSpace(_questionText)) {
+                _dialog.ShowError("Não é possível ter uma pergunta em branco!");
+                return false;
+            }
+            return true;
+        }
     }
 }
