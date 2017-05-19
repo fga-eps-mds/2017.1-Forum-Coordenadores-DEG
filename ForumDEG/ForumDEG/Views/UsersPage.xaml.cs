61bb46b1be974b1d73fa15074852a7236de5e09e
Mariana Pícolo <marianapicolo@hotmail.com>
2017-05-18 23:07:13 -0300

    Re-select item fully working for users list.
    Signed-off-by: Mariana Pícolo <marianapicolo@hotmail.com>
    Signed-off-by: Lucas Rufino <lucaslrt@outook.com>

--- ForumDEG/ForumDEG/Views/UsersPage.xaml.cs
+++ ForumDEG/ForumDEG/Views/UsersPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using ForumDEG.ViewModels;
+using System.Diagnostics;
 
 namespace ForumDEG.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -11,18 +12,16 @@ namespace ForumDEG.Views {
             InitializeComponent();
         }
 
-        override protected void OnAppearing() {
-            if (ViewModel.SelectedAdministrator != null)
+        private void ItemSelected(object sender, SelectedItemChangedEventArgs e) {
+            Debug.WriteLine("Clicked item");
+            if (e.SelectedItem != null && ViewModel.SelectAdministratorCommand != null && ViewModel.SelectAdministratorCommand.CanExecute(e)) {
+                ViewModel.SelectAdministratorCommand.Execute(e.SelectedItem);
                 ViewModel.SelectedAdministrator = null;
-            if (ViewModel.SelectedCoordinator != null)
                 ViewModel.SelectedCoordinator = null;
+            }
         }
 
-        private async void ItemSelected(object sender, SelectedItemChangedEventArgs e) {
-            await ViewModel.SelectAdministrator(e.SelectedItem as UserDetailViewModel);
-        }
-
-        private UsersViewModel ViewModel {
+    private UsersViewModel ViewModel {
             get { return (BindingContext as UsersViewModel); }
             set { BindingContext = value; }
         }
