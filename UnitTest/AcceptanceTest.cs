c8de24583bddf3692732d91e4eef70751ccb3aaa
Romeu Carvalho <romeucarvalho2009@hotmail.com>
2017-05-23 02:29:53 -0300

    [Test] Acceptance Test
    Signed-off-by: Romeu Carvalho <romeucarvalho2009@hotmail.com>
    Signed-off-by: Lucas Rufino <lucasrlt@outlook.com>

---
+++ UnitTest/AcceptanceTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+
+namespace UnitTest
+{
+    [TestFixture]
+    class AcceptanceTest
+    {
+        private string path = "";
+        private AndroidApp app;
+
+        [SetUp]
+        public void Setup()
+        {
+            app = ConfigureApp.Android.ApkFile(path).StartApp();
+        }
+
+        [Test]
+        public void CanSeeLoginButton()
+        {
+            app.Repl();
+        }
+    }
+}
