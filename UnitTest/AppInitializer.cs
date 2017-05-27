8a88dc3231e8b15f46d809c9c428d31a46845e48
Lucas Rufino <lucaslrt@outlook.com>
2017-05-26 22:19:17 -0300

    Creating Login Acceptance Test.
    Signed-off-by: Romeu Carvalho <romeucarvalho2009@hotmail.com>
    Signed-off-by: Lucas Rufino <lucaslrt@outlook.com>

---
+++ UnitTest/AppInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+
+namespace UnitTest {
+    class AppInitializer {
+        public static IApp StartApp(Platform platform) {
+
+            return ConfigureApp.Android.ApkFile("ForumDEG.ForumDEG.apk").StartApp();
+
+            //// TODO: If the iOS or Android app being tested is included in the solution 
+            //// then open the Unit Tests window, right click Test Apps, select Add App Project
+            //// and select the app projects that should be tested.
+            //if (platform == Platform.Android) {
+            //    return ConfigureApp
+            //        .Android
+            //    // TODO: Update this path to point to your Android app and uncomment the
+            //    // code if the app is not included in the solution.
+            //    .ApkFile ("ForumDEG.ForumDEG.apk")
+            //        .StartApp();
+            //}
+
+            //return ConfigureApp
+            //    .iOS
+            //// TODO: Update this path to point to your iOS app and uncomment the
+            //// code if the app is not included in the solution.
+            //    .AppBundle("../../../ForumDEG.iOS/bin/iPhoneSimulator/Debug/ForumDEG.app")
+            //    .StartApp();
+        }
+
+    }
+}
---
+++ UnitTest/LoginAcceptanceTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+
+namespace UnitTest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform) {
+            this.platform = platform;
+
+        }
+
+        [SetUp]
+        public void BeforeEachTest() {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void LoginSuccess() {
+            app.EnterText("EtRegistrationLoginPage", "123456789");
+            app.EnterText("EtPasswordLoginPage", "Pb1234567");
+            app.Tap("BtLoginLoginPage");
+
+            Assert.IsNotNull(app.Query("PageUsersPage"));
+        }
+
+        //[Test]
+        //public void LoginFailure() {
+
+        //    app.Tap("BtLoginLoginPage");
+
+        //}
+    }
+}
