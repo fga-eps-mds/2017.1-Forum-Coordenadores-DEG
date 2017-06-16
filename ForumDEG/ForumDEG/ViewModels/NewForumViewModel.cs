467b75c85107b5061b156f14e8b389a3c8c1cb76
Clarissa Borges <clarissalimab08@gmail.com>
2017-06-15 22:33:17 -0300

    Added loading spinner when creating a new forum
    Signed-off-by: Thiago Moreira <thiago.cmoreira@yahoo.com.br>
    Signed-off-by: Clarissa Borges <clarissalimab08@gmail.com>

--- ForumDEG/ForumDEG/ViewModels/NewForumViewModel.cs
+++ ForumDEG/ForumDEG/ViewModels/NewForumViewModel.cs
@@ -11,7 +11,7 @@ using Xamarin.Forms;
 using ForumDEG.Models;
 
 namespace ForumDEG.ViewModels {
-    public class NewForumViewModel {
+    public class NewForumViewModel : BaseViewModel {
         private readonly IUserDialogs _dialog;
         private readonly IPageService _pageService;
         private readonly IDatabase<Forum> _forumDB;
@@ -20,7 +20,22 @@ namespace ForumDEG.ViewModels {
         public Forum Forum { get; private set; } = new Models.Forum();
         public ICommand CancelCommand { get; private set; }
 
+        private bool _activityIndicator;
+        public bool ActivityIndicator {
+            get {
+                return _activityIndicator;
+            }
+            set {
+                if (_activityIndicator != value) {
+                    _activityIndicator = value;
+
+                    OnPropertyChanged("ActivityIndicator");
+                }
+            }
+        }
+
         public NewForumViewModel(IUserDialogs dialog, IPageService pageService, IDatabase<Forum> forumDB) {
+            ActivityIndicator = false;
             _dialog = dialog;
             _pageService = pageService;
             _forumDB = forumDB;
@@ -30,32 +45,38 @@ namespace ForumDEG.ViewModels {
         }
 
         public bool IsAnyFieldBlank() {
+            ActivityIndicator = true;
             return (String.IsNullOrWhiteSpace(Forum.Title) ||
                     String.IsNullOrWhiteSpace(Forum.Place) ||
                     String.IsNullOrWhiteSpace(Forum.Schedules));
         }
 
         public async Task<bool> CreateForum() {
+            ActivityIndicator = true;
             return (await _forumDB.Save(Forum) == 1);
         }
 
         public async Task<bool> CreateForumRemote() {
+            ActivityIndicator = true;
             return await _forumService.PostForumAsync(Forum);
         }
 
         public async void AlertSuccess() {
+            ActivityIndicator = false;
             await _dialog.AlertAsync("O fórum foi criado com sucesso. Os coordenadores serão notificados em breve."
                 , "Fórum Criado"
                 , "OK");
         }
 
         public async void SavingFailed() {
+            ActivityIndicator = false;
             await _dialog.AlertAsync("Não foi possível estabelecer conexão com o banco de dados. Por favor tente novamente."
                 , "O fórum não pôde ser criado"
                 , "OK");
         }
 
         public async void CreationFailed() {
+            ActivityIndicator = false;
             await _dialog.AlertAsync("O fórum não pôde ser criado porque existem campos que não foram preenchidos. " +
                     "Verifique e tente novamente.", 
                     "O fórum não pôde ser criado"
